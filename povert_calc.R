157f964316c06e269813b3758f5c193da1a24e05
ideodhar <129261428+ideodhar@users.noreply.github.com>
2025-01-15 20:14:03 +0530

    R files
    Poverty calculation

--- povert_calc.R
+++ povert_calc.R
@@ -2,38 +2,46 @@ library(tidyverse)
 library(data.table)
 library(zoo)
 library(priceR)
+library(ggthemes)
+library(ineq)
+library(povcalnetR)
+        
 
-
-rm(list = ls())
 setwd("D:/Thesis")
 
-cphs_exp <- fread("cphs_con_m.txt", sep = "|")
+cphs_exp_1 <- fread("cphs_con_m.txt", sep = "|")
 
+
+# filtering for wave 1
+cphs_exp <- cphs_exp_1 %>%
+  filter(grepl("May", MONTH_SLOT) | 
+           grepl("Jun", MONTH_SLOT) | 
+           grepl("Jul", MONTH_SLOT) | 
+           grepl("Aug", MONTH_SLOT))
+
+#cleanining the data
 cphs_exp <- cphs_exp %>%
-  filter(TOT_EXP != -99)
-
+  filter(TOT_EXP != -99) %>%
+  filter(RESPONSE_STATUS == "Accepted")
 cphs_exp$YEAR <- str_sub(cphs_exp$MONTH_SLOT, 4)
 cphs_exp$YEAR <- as.numeric(cphs_exp$YEAR)
+colnames(cphs_exp) <- tolower(colnames(cphs_exp))
+cphs_exp$state <- gsub(" ", "_", cphs_exp$state)
+cphs_exp$state <- tolower(cphs_exp$state)
 
-month <- cphs_exp %>%
-group_by(YEAR, HH_ID) %>%
-summarise(hh_count = n())
-
-# sample
-
-small_trial <- sample_n(cphs_exp, 100000)
-
-#making a year column 
-small_trial$YEAR <- str_sub(small_trial$MONTH_SLOT, 4)
-small_trial$YEAR <- as.numeric(small_trial$YEAR)
-#colnames(small_trial)[8] <- "year"
-colnames(small_trial) <- tolower(colnames(small_trial))
+# check 
+cphs_exp|>
+  filter(year == 2021) |>
+mutate(
+adj_w = hh_nr_for_country_ms *  r_hh_wgt_for_country_ms
+)|>
+dplyr::pull(adj_w) %>%
+  sum()
 
 
+# Making class of hh_size from continues to discreet 
 
-#size of the household
-
-small_trial <- small_trial %>%
+cphs_exp <- cphs_exp %>%
   mutate(size_group = case_when(
     size_group == "8-10 Members" ~ 9,
     size_group == "11-15 Members" ~ 13,
@@ -47,63 +55,97 @@ small_trial <- small_trial %>%
 inflation_dataframe <- retrieve_inflation_data("IN")
 countries_dataframe <- show_countries()
 
-#adjusting tot_exp
-small_trial <- small_trial %>%
+adjusted_values <- numeric(nrow(cphs_exp))
+
+for (i in unique(cphs_exp$year)) {
+  rows_to_update <- cphs_exp$year == i
+  adjusted_values[rows_to_update] <- adjust_for_inflation(
+    cphs_exp$tot_exp[rows_to_update],
+    i,
+    "IN",
+    to_date = 2014,
+    inflation_dataframe = inflation_dataframe,
+    countries_dataframe = countries_dataframe
+  )
+}
+cphs_exp$real_tot_exp <- adjusted_values
+
+##adj weights 
+
+cphs_exp <- cphs_exp %>%
+  group_by(month_slot) %>%
   mutate(
-  real_tot_exp = adjust_for_inflation(tot_exp, 
-                                      year, 
-                                      "IN", 
-                                      2014, 
-                                      inflation_dataframe = inflation_dataframe, 
-                                      countries_dataframe = countries_dataframe)
-)
+    adj_s = hh_nr_for_state_ms *  r_hh_wgt_for_state_ms,
+    adj_c = hh_nr_for_country_ms * r_hh_wgt_for_country_ms,
+    s = r_hh_wgt_for_state_ms * hh_nr_for_state_ms, 
+    c = r_hh_wgt_for_country_ms * hh_nr_for_country_ms
+  )
 
 
-### poverty calculation 
-small_trial <- small_trial %>%
-  group_by(year, hh_id) %>%
-  mutate(hh_me = mean(real_tot_exp)) 
-
-## test 
-
-hh_pre_count <- small_trial %>%
-  group_by(hh_id, year) %>%
-  summarise(count = n())
-
-hh_pre_count <- hh_pre_count %>%
-  filter(count > 1)
-
-hh_id_dont_match <- small_trial[small_trial$real_tot_exp!=small_trial$hh_me,] 
-
-hh_post_count <- hh_id_dont_match %>%
-  group_by(hh_id, year) %>%
-  summarise(count = n()) %>%
-  filter(count > 1)
-
-nrow(intersect(hh_post_count, hh_pre_count))/nrow(hh_pre_count)
 
 #calculating HH per capita expenditure
 
-small_trial<- small_trial %>%
-  group_by(district, year, hh_id) %>%
+cphs_exp <- cphs_exp %>%
+  group_by(year, hh_id) %>%
   mutate(
-    hhi_mpce = hh_me/size_group 
+    hhi_mpce = real_tot_exp/size_group 
   ) 
 
-# Create a new column 'dummy_POOR' based on the poverty thresholds
-small_trial <- small_trial %>%
-  mutate(poor = ifelse(region_type == "URBAN" & hhi_per_capita <= 1407 | region_type == "RURAL" & hhi_per_capita <= 972,
+
+## calculating mpce 
+
+mpce_dist<- cphs_exp%>%
+  group_by(year, district) %>%
+  summarise(district_mpce = DescTools::Mean(hhi_mpce))
+
+
+##ayush
+ayush <- cphs_exp|>
+  filter(response_status == "Accepted")|>
+  filter(month_slot %in%  c( "Sep 2021" ,"Oct 2021" ,
+                             "Nov 2021" ,"Dec 2021"))|>
+  mutate(
+    adj_w = hh_nr_for_country_ms *  r_hh_wgt_for_country_ms
+  )|> 
+  group_by(state,year)|>
+  summarise(
+    
+    average_exp = DescTools::Mean(
+      real_tot_exp,
+      weights = adj_w
+    )
+  )
+
+mpce_state<- cphs_exp%>%
+  group_by(year, state) %>%
+  summarise(state_mpce = DescTools::Mean(hhi_mpce, weights = adj_s),
+            state_mpce_cphs = DescTools::Mean(hhi_mpce, weights = s))
+
+write.csv(mpce_state, "state_mpce.csv")
+
+###headcount
+
+#Create a new column poor based on the poverty thresholds
+cphs_exp <- cphs_exp %>%
+  mutate(poor = ifelse(region_type == "URBAN" & hhi_mpce <= 1407 | region_type == "RURAL" & hhi_mpce <= 972,
                        1, 0))
 
 
-# Multiply 'dummy_POOR' with 'size_group' to get the number of poor people
- small_trial %>%
-  group_by(year, district) %>%
-   summarize(tot_pop = sum(size_group),
-             poor_pop = poor * size_group,
-             headcount_ratio = poor_pop/tot_pop) %>%
-   view()
- 
+#state poverty
+
+poverty_headcount <- cphs_exp %>%
+  group_by(year, state) %>%
+  summarise(headcount_ratio = DescTools::Mean(poor, weights = adj_s))
+           # rural_headcount = DescTools::Mean(poor[region_type == "RURAL"], weights = adj_s[region_type == "RURAL"]),
+          #  urban_headcount = DescTools::Mean(poor[region_type == "URBAN"], weights = adj_s[region_type == "URBAN"])) 
+
+fwrite(poverty_headcount, "poverty_state_nw.csv")
+
+poverty_headcount_year <- cphs_exp %>%
+  group_by(year) %>%
+  summarise(headcount_ratio = DescTools::Mean(poor, weights = adj_c), 
+            rural_headcount = DescTools::Mean(poor[region_type == "RURAL"], weights = adj_s[region_type == "RURAL"]),
+            urban_headcount = DescTools::Mean(poor[region_type == "URBAN"], weights = adj_s[region_type == "URBAN"]))
 
 
 
